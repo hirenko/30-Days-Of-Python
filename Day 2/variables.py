b2c2e7d6c7c50d97d0eebbdf7ebedafb4795d62a
hirenko <volodimir.hirenko@gmail.com>
2026-06-16 19:16:54 -0400

    Day 2

---
+++ Day 2/variables.py
@@ -0,0 +1,42 @@
+print ("Day 2: 30 Days of python programming")
+first_name = "Alesha"
+last_name = "Popovich"
+full_name = "Alesha Popovich"
+country = "United States"
+city = "Miami"
+age = 250
+year = 1776
+is_married = True
+is_true = True
+is_light_on = True
+test1 = "a1"
+test2 = "a2"
+test3 = "a3"
+print (type(first_name))
+print (len(first_name))
+print (len(first_name + last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+variable_diff = num_two - num_one
+variable_product = num_two * num_one
+variable_division = num_one / num_two
+remainder = num_two % num_one
+print (remainder)
+variable_exp = num_one ** num_two
+print (variable_exp)
+floor_division = num_one // num_two
+print (floor_division)
+pi = 3.14159
+radius = float(input("Enter radius: "))
+area_of_circle = pi * (radius ** 2)
+print(area_of_circle)
+circum_of_circle = 2 * pi * radius
+print(circum_of_circle)
+
+firstName = input("Enter your First Name: ")
+lastName = input("Enter your Last Name: ")
+countryName = input("Enter your country: ")
+age = input("Enter your age: ")
+print(firstName, lastName, countryName, age)
+help('keywords')
